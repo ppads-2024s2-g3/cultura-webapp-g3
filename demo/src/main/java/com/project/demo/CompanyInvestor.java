c99eb1297b7629f41dde2fcd59b38d4d542f03e3
Felipe Damasceno <45994861+Feedamasceno@users.noreply.github.com>
2024-10-11 00:43:27 +0000

    ajustando investor.java e companyinvestor.java

--- demo/src/main/java/com/project/demo/CompanyInvestor.java
+++ demo/src/main/java/com/project/demo/CompanyInvestor.java
@@ -1,34 +1,18 @@
+package io.github.pravnkr.jpainheritence.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 @Entity
 @DiscriminatorValue("INTERNAL")
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class CompanyInvestor extends Investor {
-    private String companyType;
+  private String companyType;
 
-    @Column(unique = true)
-    private String lei;
-}
-
-@Entity
-@DiscriminatorValue("LP")
-@Data
-@EqualsAndHashCode(callSuper = true)
-public class FundLP extends Investor {
-    private String lpType;
-}
-
-@Entity
-@DiscriminatorValue("CO_INVESTOR")
-@Data
-@EqualsAndHashCode(callSuper = true)
-public class CoInvestor extends Investor {
-    private String coInvestorType;
-}
-
-@Entity
-@DiscriminatorValue("LENDER")
-@Data
-@EqualsAndHashCode(callSuper = true)
-public class Lender extends Investor {
-    private String lenderType;
+  @Column(unique = true)
+  private String lei;
 }
--- demo/src/main/java/com/project/demo/Investor.java
+++ demo/src/main/java/com/project/demo/Investor.java
@@ -1,13 +1,18 @@
+package io.github.pravnkr.jpainheritence.models;
+
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype", discriminatorType = DiscriminatorType.STRING)
 @Data
 @EqualsAndHashCode(callSuper = true)
 public abstract class Investor extends AbstractEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
-    private String name;
+  private String name;
 }
-
